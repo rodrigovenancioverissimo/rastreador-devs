39cfc89acd8b05fef4a96bf0063ad7e7a53c3479
Rodrigo V Veríssimo <rodrigovenancioverissimo@gmail.com>
2020-02-27 11:32:49 -0400

    Criado frontend web template

---
+++ web/src/App.js
@@ -0,0 +1,69 @@
+import React from 'react'
+
+import './global.css'
+import './App.css'
+import './Sidebar.css'
+import './Main.css'
+
+function App() {
+  return (
+  <div id="app">
+    <aside>
+      <strong>Cadastrar</strong>
+      <form>
+        <div className="input-block">
+          <label htmlFor="github_username">Usuário do Github</label>
+          <input name="github_username" id="github_username" required />
+        </div>
+
+        <div className="input-block">
+          <label htmlFor="techs">Tecnologías</label>
+          <input name="techs" id="techs" required />
+        </div>
+
+        <div className="input-group">
+          <div className="input-block">
+            <label htmlFor="latitude">Latitude</label>
+            <input name="latitude" id="latitude" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="longitude">Longitude</label>
+            <input name="longitude" id="longitude" required />
+          </div>
+        </div>
+
+        <button type="submit">Salvar</button>
+      </form>
+    </aside>
+    <main>
+      <ul>
+        <li className="dev-item">
+          <header>
+            <img src="https://avatars2.githubusercontent.com/u/17599326?v=4" alt="Avatar Githunb"/>
+            <div className="user-info">
+              <strong>Rodrigo V Verissimo</strong>
+              <span>React, Ruby</span>
+            </div>
+          </header>
+          <p>Amo computação</p>
+          <a href="https://github.com/rodrigovenancioverissimo">Acessar perfil no Github</a>
+        </li>
+        <li className="dev-item">
+          <header>
+            <img src="https://avatars2.githubusercontent.com/u/17599326?v=4" alt="Avatar Githunb"/>
+            <div className="user-info">
+              <strong>Rodrigo V Verissimo</strong>
+              <span>React, Ruby</span>
+            </div>
+          </header>
+          <p>Amo computação</p>
+          <a href="https://github.com/rodrigovenancioverissimo">Acessar perfil no Github</a>
+        </li>
+      </ul>
+    </main>
+  </div>
+  );
+}
+
+export default App;
